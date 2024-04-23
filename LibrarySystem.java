56efa69beef9a6dfd0e98324ff41e27677ffa6a8
Seon-M <tjswo0428@naver.com>
2024-04-23 16:27:42 +0900

    중간고사 실기 제출

--- LibrarySystem.java
+++ LibrarySystem.java
@@ -1,10 +1,18 @@
 public class LibrarySystem {
 	// 변수 정의
+	private Book[] booklist;
+	private static int bookCount = 0;
+	private static final int NUM_BOOK = 10;
 	
-	// 생성자 정
+	// 생성자 정의
+	public LibrarySystem() {
+	    this.booklist = new Book[NUM_BOOK]; // 생성자
+	}
 
+	
 	public void addBook(Book book) {
 		// 책 추가
+		booklist [bookCount++] = book;
 		// 도움을 위해 Cart.java의 insertBook(Book book)을 참조하세요
 	}
 	
@@ -33,37 +41,62 @@ public class LibrarySystem {
 	
 	public void borrowBook(String title) {
         // `for` LibrarySystem의 모든 Book {
+		for ( int i = 0; i < bookCount; i++ ) {
             // `if` 책 제목이 발견되면 {
+			if ( booklist[i].getTitle().equals(title)) {
                 // `if` 책이 있다면 {
-                    // setAvailable을 false로 설정하고 메시지를 출력합니다.
-				// } `else` 책이 없다면 {
+				  if (booklist[i].isAvailable()) {
+	                    // setAvailable을 false로 설정하고 메시지를 출력합니다.
+					  booklist[i].setAvailable(false);
+		                System.out.println("책 '" + title + "' 대출했습니다.");
+				  } else {
+					  System.out.println("책 '" + title + "'가 현재 없습니다.");
+		            }
+				  
 					// 메시지를 인쇄하다
 				// }
 				return; // 책 제거 후 종료 방법
 			// }
-		// }
+		}
 		// 찾을 수 없으면 메시지를 인쇄합니다.
 		System.out.println("# 오류: '" + title + "' 못 찾았습니다!");
+		}
 	}
 	
 	public void returnBook(String title) {
 	    // `for` LibrarySystem의 모든 Book {
+		for ( int i = 0; i < bookCount; i++ ) {
             // `if` 책 제목이 발견되면 {
+			if ( booklist[i].getTitle().equals(title)) {
                 // `if` 책이 없다면 {
+				if (!booklist[i].isAvailable()) {
+				  booklist[i].setAvailable(true);
                     // setAvailable을 true로 설정하고 메시지를 출력합니다.
+				  System.out.println("책 '" + title + "' 를 반납했습니다.");
 				// } `else` 책이 있다면 {
+				} else  {
+					System.out.println("책 '" + title + "'가 현재 없습니다.");
+					
 					// 메시지를 인쇄하다
-				// }
+				}
 				return; // 책 제거 후 종료 방법
-			// }
+			}
 		// }
 		// 찾을 수 없으면 메시지를 인쇄합니다.
 		System.out.println("# 오류: '" + title + "' 못 찾았습니다!");
+		}
 	}
 	
 	public void displayAllBooks() {
 		// 책 목록 출력 (책이름, 저자, 출판년도, 대출가능여부 출력)
 
+		for(int i =0; i < bookCount; i++) {
+			System.out.print(booklist[i].getIsbn() + " | ");
+			System.out.print(booklist[i].getTitle() + " | ");
+			System.out.print(booklist[i].getAuthor() + " | ");
+			System.out.print(booklist[i].getYear() + " | ");
+			System.out.println();
+		}
         /* 
         Expected output:
         "ISBN1234 | JSP 웹 프로그래밍 | 송미영 | 2018 | Available"
